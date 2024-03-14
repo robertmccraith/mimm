1a777f4fe3939bd99998d84a6013c85ed6d0fba7
Robert McCraith <mccraithrobert@gmail.com>
2024-03-14 20:20:43 +0000

    fix: setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.MD").read_text()
 
 setup(
     name="mimm",
