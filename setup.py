dba27aca7dfff6c97ac674585f7aa1a5bb565e32
Robert McCraith <mccraithrobert@gmail.com>
2024-03-14 19:45:12 +0000

    fix: pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="mimm",
-    version="1.0",
     description="MLX Image Models",
     author="Robert McCraith",
     author_email="mccraithrobert@gmail.com",
