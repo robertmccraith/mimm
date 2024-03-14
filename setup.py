1b78f423ee47698161c3f8a400bb87d07c1e32d2
Robert McCraith <mccraithrobert@gmail.com>
2024-03-14 20:18:59 +0000

    fix: setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="mimm",
@@ -10,6 +13,6 @@ setup(
     install_requires=[
         "mlx"
     ],
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
 )
